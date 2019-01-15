9559f5990e0928e7f7948b8dfd662fb52374339c
linjianghe <linjianghe@tencent.com>
2019-01-15 12:31:19 +0800

    feat: 创建项目

--- main-process/business/create-project.js
+++ main-process/business/create-project.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { ipcMain, dialog } = require('electron');
 const fse = require('fs-extra');
+const { initProject } = require('mockstar-generators');
 
 const { EVENT } = require('../../src/business/electron-main-render-common');
 
@@ -10,13 +11,32 @@ const { EVENT } = require('../../src/business/electron-main-render-common');
  * @param {Object} event 事件对象
  * @param {Object} [opts] 额外的参数，用于处理某些逻辑
  */
-ipcMain.on(EVENT.CREATE_PROJECT.REQ, (event, opts) => {
-    const data = require(path.join(__dirname, '../../src/business/mock/create-project-result'))(opts);
+ipcMain.on(EVENT.CREATE_PROJECT.REQ, (event, opts = {}) => {
+    // 初始化
+    initProject({
+        isDev: false,
+        parentPath: opts.parentPath,
+        name: opts.name,
+        port: opts.port
+    })
+        .then((data) => {
+            // console.log(data);
+            // TODO 临时测试
+            fse.outputJsonSync(path.join(__dirname, '../../tmp/create-project.json'), opts);
 
-    fse.outputJsonSync(path.join(__dirname, '../../tmp/create-project.json'), data);
-
-    event.sender.send(EVENT.CREATE_PROJECT.RSP, {
-        retcode: 0,
-        result: data.result
-    }, opts);
+            return {
+                retcode: 0
+            };
+        })
+        .catch((err) => {
+            console.error(err);
+            return {
+                retcode: -1,
+                errmsg: err
+            };
+        })
+        .then((data) => {
+            // console.log(data);
+            event.sender.send(EVENT.CREATE_PROJECT.RSP, data, opts);
+        });
 });
