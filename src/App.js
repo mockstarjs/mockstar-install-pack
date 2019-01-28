6da39270147aa63bc62bc89279b4e454d1dc2fae
helinjiang <onehlj@qq.com>
2019-01-28 19:57:52 +0800

    feat: 隐藏导航

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route,Switch,Redirect } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import { Layout } from 'antd';
 
